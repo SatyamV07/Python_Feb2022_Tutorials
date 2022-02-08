b273d3db0891e26ae5d701fd159bbe2a7c3c3be9
Kumaran R <37325822+kumar1987an@users.noreply.github.com>
2022-02-08 13:09:26 +0530

    08-02-2022

---
+++ Python_Basics_Session/codes/namespace.py
@@ -0,0 +1,34 @@
+print(dir())
+import math
+
+print()
+
+print(dir())
+print()
+print(math.sin(676))
+
+x = 77
+
+print()
+print(dir())
+
+
+def add_me_to_the_namespace():
+    pass
+
+
+print()
+print(dir())
+
+print()
+
+
+class Alpha:
+    pass
+
+
+print(dir())
+
+print()
+
+print(globals())
